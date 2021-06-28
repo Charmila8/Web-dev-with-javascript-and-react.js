fe5599aafcae6ede0d0f409533e80dcc0e626273
Charmila8 <86521294+Charmila8@users.noreply.github.com>
2021-06-28 13:44:06 +0530

    Update Info.jsx

--- Info.jsx
+++ Info.jsx
@@ -4,10 +4,9 @@ function info(){
   return(
     <div>
       <h1>react.js and note.js<\h1>
-      <p> it was an really amazing experiance
-        <\p>
+      <p> it was an really amazing experiance <\p>
         <\div>
         );
         }
-        export defult info;
+        export default info;
         
