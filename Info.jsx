903afab5a80e4efab551a79dcdf75ddb39d00b32
Charmila8 <86521294+Charmila8@users.noreply.github.com>
2021-06-28 13:45:35 +0530

    Update Info.jsx

--- Info.jsx
+++ Info.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function info(){
   return(
-    <div>
+    <div className="note">
       <h1>react.js and note.js<\h1>
       <p> it was an really amazing experiance <\p>
         <\div>
